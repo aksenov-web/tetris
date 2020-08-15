8e284800ca4b40120177fa14aae2f6b1c671f42a
aksenov-web <aksenov@inbox.lv>
2020-08-15 23:14:18 +0300

    Tetris

---
+++ figure_manager.js
@@ -0,0 +1,303 @@
+let figureManager = {
+  main_field: null,
+  x: 0,
+  y: 3,
+
+
+  figureFall: function(y){
+    let obj = this;
+    setTimeout(function(){
+      if (y <=20) {
+//        obj.createFigure1(5, y++, 1);
+//        obj.createFigure2(5, y++, 1);
+//        obj.createFigure3(5, y++, 1);
+//        obj.createFigure4(5, y++, 1);
+//        obj.createFigure5(5, y++, 1);
+//        obj.createFigure6(5, y++, 1);
+        obj.createFigure7(5, y++, 2);
+        obj.figureFall(y);
+      }
+    }, 500);
+},
+figureLeft: function(x){
+    this.createFigure1(x--, 5, 1);
+    this.figureLeft(x);
+
+  },
+
+  //палка
+  createFigure1: function (x, y, r) {
+    let color = "";
+
+    for (let n = 1; n <= 2; n++) {
+       if (n == 1) {
+        color = "";
+       }
+      else {
+        color = "aqua"
+      }
+        this.cellColorChange(this.x, this.y, color);
+
+      switch (r) {
+        case 1:
+        case 3:
+          this.cellColorChange(this.x, --this.y, color);
+          this.cellColorChange(this.x, --this.y, color);
+          this.cellColorChange(this.x, --this.y, color);
+          this.x = x;
+          this.y = y;
+        break;
+        case 2:
+        case 4:
+          this.cellColorChange(++this.x, this.y, color);
+          this.cellColorChange(++this.x, this.y, color);
+          this.cellColorChange(++this.x, this.y, color);
+          this.x = x;
+          this.y = y;
+        break;
+      }
+    }
+  },
+
+  // z
+   createFigure2: function (x, y, r) {
+     let color = "";
+
+    for (let n = 1; n <= 2; n++) {
+       if (n == 1) {
+        color = "";
+       }
+      else {
+        color = "red"
+      }
+    switch(r) {
+      case 1:
+      case 3:
+        this.cellColorChange(this.x, this.y-1, color);
+        this.cellColorChange(++this.x, this.y-1, color);
+        this.cellColorChange(this.x, this.y, color);
+        this.cellColorChange(++this.x, this.y, color);
+        this.x = x;
+        this.y = y;
+      break;
+      case 2:
+      case 4:
+        this.cellColorChange(this.x, this.y, color);
+        this.cellColorChange(this.x, --this.y, color);
+        this.cellColorChange(++this.x, this.y, color);
+        this.cellColorChange(this.x, --this.y, color);
+        this.x = x;
+        this.y = y;
+      break;
+    }
+    }
+  },
+
+  // backward z
+   createFigure3: function (x, y, r) {
+     let color = "";
+     for (let n = 1; n <= 2; n++) {
+       if (n == 1) {
+        color = "";
+       }
+      else {
+        color = "green"
+      }
+    switch(r) {
+      case 1:
+      case 3:
+        this.cellColorChange(this.x, this.y, color);
+        this.cellColorChange(++this.x, this.y, color);
+        this.cellColorChange(this.x, --this.y, color);
+        this.cellColorChange(++this.x, this.y, color);
+        this.x = x;
+        this.y = y;
+      break;
+      case 2:
+      case 4:
+        this.cellColorChange(this.x+1, this.y, color);
+        this.cellColorChange(this.x+1, --this.y, color);
+        this.cellColorChange(this.x, this.y, color);
+        this.cellColorChange(this.x, --this.y, color);
+        this.x = x;
+        this.y = y;
+      break;
+    }
+    }
+  },
+
+  // square
+  createFigure4:  function (x, y, r) {
+    let color = "";
+    for (let n = 1; n <= 2; n++) {
+       if (n == 1) {
+        color = "";
+       }
+      else {
+        color = "yellow"
+      }
+        this.cellColorChange(this.x, this.y, color);
+        this.cellColorChange(this.x+1, this.y, color);
+        this.cellColorChange(this.x, this.y-1, color);
+        this.cellColorChange(this.x+1, this.y-1, color);
+      this.x = x;
+      this.y = y;
+    }
+
+    },
+
+  // T
+  createFigure5: function (x, y, r) {
+    let color = "";
+    for (let n = 1; n <= 2; n++) {
+       if (n == 1) {
+        color = "";
+       }
+      else {
+        color = "purple"
+      }
+    switch(r) {
+      case 1:
+        this.cellColorChange(this.x, this.y, color);
+        this.cellColorChange(this.x+1, this.y, color);
+        this.cellColorChange(this.x+2, this.y, color);
+        this.cellColorChange(this.x+1, this.y-1, color);
+        this.x = x;
+        this.y = y;
+      break;
+      case 2:
+        this.cellColorChange(this.x+1, this.y, color);
+        this.cellColorChange(this.x+1, this.y-1, color);
+        this.cellColorChange(++this.x, this.y-2, color);
+        this.cellColorChange(++this.x, this.y-1, color);
+        this.x = x;
+        this.y = y;
+      break;
+      case 3:
+        this.cellColorChange(this.x+1, this.y, color);
+        this.cellColorChange(this.x+1, this.y-1, color);
+        this.cellColorChange(this.x+1, this.y-2, color);
+        this.cellColorChange(this.x, this.y-1, color);
+        this.x = x;
+        this.y = y;
+      break;
+      case 4:
+        this.cellColorChange(this.x, this.y-1, color);
+        this.cellColorChange(this.x+1, this.y-1, color);
+        this.cellColorChange(this.x+2, this.y-1, color);
+        this.cellColorChange(this.x+1, this.y, color);
+        this.x = x;
+        this.y = y;
+      break;
+    }
+    }
+  },
+
+  // г
+  createFigure6: function (x, y, r) {
+    let color = "";
+
+    for (let n = 1; n <= 2; n++) {
+       if (n == 1) {
+        color = "";
+       }
+      else {
+        color = "blue"
+      }
+      switch(r) {
+        case 1:
+          this.cellColorChange(this.x, this.y, color);
+          this.cellColorChange(this.x, --this.y, color);
+          this.cellColorChange(this.x, --this.y, color);
+          this.cellColorChange(this.x+1, this.y, color);
+          this.x = x;
+          this.y = y;
+        break;
+        case 2:
+          this.cellColorChange(this.x, this.y-1, color);
+          this.cellColorChange(++this.x, this.y-1, color);
+          this.cellColorChange(++this.x, this.y-1, color);
+          this.cellColorChange(this.x, this.y, color);
+          this.x = x;
+          this.y = y;
+        break;
+        case 3:
+          this.cellColorChange(this.x, this.y, color);
+          this.cellColorChange(++this.x, this.y, color);
+          this.cellColorChange(this.x, --this.y, color);
+          this.cellColorChange(this.x, --this.y, color);
+          this.x = x;
+          this.y = y;
+        break;
+        case 4:
+          this.cellColorChange(this.x, this.y-1, color);
+          this.cellColorChange(this.x, this.y, color);
+          this.cellColorChange(this.x+1, this.y, color);
+          this.cellColorChange(this.x+2, this.y, color);
+          this.x = x;
+          this.y = y;
+        break;
+      }
+    }
+  },
+
+  // обратная г
+  createFigure7: function (x, y, r) {
+    let color = "";
+    for (let n = 1; n <= 2; n++) {
+       if (n == 1) {
+        color = "";
+       }
+      else {
+        color = "orange"
+      }
+      switch(r) {
+        case 1:
+          this.cellColorChange(this.x, this.y-2, color);
+          this.cellColorChange(this.x+1, this.y-2, color);
+          this.cellColorChange(this.x+1, this.y-1, color);
+          this.cellColorChange(this.x+1, this.y, color);
+          this.x = x;
+          this.y = y;
+        break;
+        case 2:
+          this.cellColorChange(this.x, this.y, color);
+          this.cellColorChange(this.x+1, this.y, color);
+          this.cellColorChange(this.x+2, this.y, color);
+          this.cellColorChange(this.x+2, this.y-1, color);
+          this.x = x;
+          this.y = y;
+        break;
+        case 3:
+          this.cellColorChange(this.x, this.y, color);
+          this.cellColorChange(this.x+1, this.y, color);
+          this.cellColorChange(this.x, this.y-1, color);
+          this.cellColorChange(this.x, this.y-2, color);
+          this.x = x;
+          this.y = y;
+        break;
+        case 4:
+          this.cellColorChange(this.x, this.y, color);
+          this.cellColorChange(this.x, this.y-1, color);
+          this.cellColorChange(this.x+1, this.y-1, color);
+          this.cellColorChange(this.x+2, this.y-1, color);
+          this.x = x;
+          this.y = y;
+        break;
+      }
+    }
+  },
+
+
+  cellColorChange: function (x, y, color, r) {
+    if (y < 1 || y > 20 || x < 1 || x >10) {
+        return;
+        }
+
+    let nth = x + (10 * y) - 10;
+
+    let cell = this.main_field.querySelector('div:nth-child(' + nth + ')');
+    cell.style.background = color;
+
+    }
+}
--- script.js
+++ script.js
@@ -1,80 +1,101 @@
 
 let main_field = document.getElementById('main-field');
+let emptyDiv = document.createElement('div');
+
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.code);
+  switch (e.code) {
+    case "ArrowLeft":
+      figureManager.figureLeft;
+      console.log(figureManager.figureLeft);
+    break;
+    case "ArrowRight":
+      console.log("To the right!");
+    break;
+    case "Space":
+      console.log("Rotation");
+    break;
+    case "ArrowDown":
+      figureManager.figureFastFall;
+      console.log("Fast Fall");
+    break;
+  }
+
+});
+
+/*
+* Add empty div tags
+*/
+for (let i = 0; i < 200; i++) {
+  main_field.appendChild(
+    emptyDiv.cloneNode()
+  );
+    document.getElementById('test-field').appendChild(
+    document.createElement('div')
+  );
+
+}
+
+figureManager.main_field = main_field;
+// figureManager.createFigure1(5, 5, 1);
 
 
 
-/*let x = 1;
+figureManager.figureFall(0);
 
-for (let y = 1; y <= 20; y++)
-  {
-    cellColorChange(x,y, "blue");
-    }
+
+//figureManager.createFigure1(5, 4, 1);
+//figureManager.createFigure2(5, 3, 1);
+//figureManager.createFigure3(2, 3, 1);
+//figureManager.createFigure4(5, 2, 1);
+//figureManager.createFigure5(5, 3, 1);
+//figureManager.createFigure6(5, 3, 4);
+//figureManager.createFigure7(5, 3, 4);
+
+/*
+figureManager.main_field = document.getElementById('test-field');
+figureManager.createFigure1(5, 4, 1);
+//figureManager.createFigure2(5, 3, 1);
+//figureManager.createFigure3(2, 3, 1);
+//figureManager.createFigure4(5, 2, 1);
+//figureManager.createFigure5(5, 3, 1);
+//figureManager.createFigure6(5, 3, 4);
+//figureManager.createFigure7(5, 3, 4);
 */
 
-createFigure1(2, 2);
-createFigure2(6, 2);
-createFigure3(2, 6);
-createFigure4(6, 5);
-createFigure5(2, 10);
-createFigure6(6, 10);
-createFigure7(9, 10);
+// object
+/*let cars = {
+  audi: {
+    model: 32,
+    gas: 324
+  },
+  subaru: 2255,
+  getColor: function (color) {;
+    return "This is color is " + color;
 
-function createFigure1(x, y) {
-  cellColorChange(x, y, 'blue');
-  cellColorChange(x, y+1, 'blue');
-  cellColorChange(x, y+2, 'blue');
-  cellColorChange(x, y+3, 'blue');
-}
-
-function createFigure2(x, y) {
-  cellColorChange(x, y, 'white');
-  cellColorChange(x+1, y, 'white');  
-  cellColorChange(x+1, y+1, 'white');  
-  cellColorChange(x+2, y+1, 'white');
-}
-
-function createFigure3(x, y) {
-  cellColorChange(x+1, y, 'lightgrey');
-  cellColorChange(x, y+1, 'lightgrey');  
-  cellColorChange(x+1, y+1, 'lightgrey');  
-  cellColorChange(x+2, y, 'lightgrey');
-}
-
-function createFigure4(x, y) {
-  cellColorChange(x, y, 'red');
-  cellColorChange(x+1, y, 'red');  
-  cellColorChange(x, y+1, 'red');  
-  cellColorChange(x+1, y+1, 'red');
-}
-
-function createFigure5(x, y) {
-  cellColorChange(x+1, y, 'green');
-  cellColorChange(x, y+1, 'green');  
-  cellColorChange(x+1, y+1, 'green');  
-  cellColorChange(x+2, y+1, 'green');
-}
-
-function createFigure6(x, y) {
-  cellColorChange(x, y, 'black');
-  cellColorChange(x+1, y, 'black');  
-  cellColorChange(x, y+1, 'black');  
-  cellColorChange(x, y+2, 'black');
-}
-
-function createFigure7(x, y) {
-  cellColorChange(x, y, 'purple');
-  cellColorChange(x+1, y, 'purple');  
-  cellColorChange(x+1, y+1, 'purple');  
-  cellColorChange(x+1, y+2, 'purple');
-}
-
-function cellColorChange(x, y, color) {
-  
-  let nth = x + (10 * y) - 10;
-  let cell = main_field.querySelector('div:nth-child(' + nth + ')');
-  
-  cell.style.background = color;
   }
+};
+
+console.log(cars.getColor("black"));
+/*
+
+
+// prototype
+let cars = function() {
+  this.specification = {
+    model: 32,
+    gas: 324
+  };
+};
+
+let audi = new cars();
+audi.specification.gas = "diesel";
+console.log(audi.specification);
+
+let bmw = new cars();
+bmw.specification.gas = "gasoline";
+console.log(bmw.specification);
 
 /*game objects:
 line x,y; x,y+1; x,y+2; x,y+3;
